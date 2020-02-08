8f449332e3eee384470e63524023fc8ba128ea18
daveedson <daveeddrumz@gmail.com>
2020-02-08 18:27:55 +0100

    Splash screen creation and Thread creation

---
+++ app/src/main/java/com/example/teamwork/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.teamwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(8000);
+                    Intent intent = new Intent(getApplicationContext(),Home.class);
+                    startActivity(intent);
+                    finish();
+
+                }catch (InterruptedException e){
+
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        };
+        thread.start();
+    }
+
+
+
+}
